aaa551090110a63bf8f0d4e49c1f40241ab93122
alejandro <alejo4373@gmail.com>
2019-05-19 23:06:04 -0400

    database schema [WIP]

---
+++ db/production.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS production;
+CREATE DATABASE production;
+
+\c production;
+
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  text VARCHAR NOT NULL,
+  completed BOOLEAN DEFAULT FALSE,
+  points INT NOT NULL
+);
