2eccf68adab0662cffea22d4d348df3f681395cd
Alejo <alejandrofranco@pursuit.org>
2019-07-12 13:53:11 -0400

    allow adding tags to je

--- db/api.js
+++ db/api.js
@@ -24,8 +24,27 @@ const updateTodo = (id, todoEdits) => {
   return db.one(updateQuery, {id})
 }
 
-const addJournalEntry = (entry) => {
-  return db.one('INSERT INTO journal_entries(text) VALUES(${text}) RETURNING *', entry)
+const addJournalEntry = async (entry) => {
+  const { tag_ids } = entry;
+  try {
+    const journalEntry = await db.one('INSERT INTO journal_entries(text) VALUES(${text}) RETURNING *', entry)
+    const values = tag_ids.map(tag_id => ({
+        'tag_id': tag_id,
+        'je_id': journalEntry.id
+    }))
+
+    const columnSet = new helpers.ColumnSet(['je_id', 'tag_id'], { table: 'je_tags' })
+    const query = helpers.insert(values, columnSet)
+
+    await db.none(query)
+
+    return {
+      ...journalEntry,
+      tag_ids: tag_ids
+    }
+  } catch (err) {
+    throw err;
+  }
 }
 
 const createTag = (name) => {
