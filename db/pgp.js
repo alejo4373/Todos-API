28dd50ca0377c75e51fc4c344a612eee4b9a611a
Alejo <alejandrofranco@pursuit.org>
2019-09-10 12:19:39 -0400

    change db url

--- db/pgp.js
+++ db/pgp.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-const connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/production";
+const connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/todos_api_db";
 module.exports = {
   helpers: pgp.helpers, 
   errors: pgp.errors,
