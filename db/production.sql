ded8dacb52d24ff732609040daf42c558f11ba20
Alejo <alejandrofranco@pursuit.org>
2019-09-10 10:27:09 -0400

    db edits to ref todos and je by username + remove unused

--- db/production.sql
+++ db/production.sql
@@ -1,38 +1,23 @@
-DROP DATABASE IF EXISTS production;
-CREATE DATABASE production;
+DROP DATABASE IF EXISTS todos_api_db;
+CREATE DATABASE todos_api_db;
 
-\c production;
+\c todos_api_db;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  username VARCHAR UNIQUE NOT NULL, 
-  password_digest VARCHAR,
-  points INT
+  username VARCHAR UNIQUE NOT NULL 
 );
 
 CREATE TABLE todos (
   id SERIAL PRIMARY KEY,
-  owner_id INT REFERENCES users(id),
+  owner VARCHAR REFERENCES users(username),
   text VARCHAR NOT NULL,
-  completed BOOLEAN DEFAULT FALSE,
-  value INT NOT NULL
+  completed BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE journal_entries (
   id SERIAL PRIMARY KEY,
-  owner_id INT REFERENCES users(id),
+  owner VARCHAR REFERENCES users(username),
   text VARCHAR NOT NULL,
   ts TIMESTAMPTZ DEFAULT NOW()
-);
-
-CREATE TABLE tags (
-  id SERIAL PRIMARY KEY,
-  owner_id INT REFERENCES users(id),
-  name VARCHAR (25)
-);
-
-CREATE TABLE je_tags (
-  id SERIAL PRIMARY KEY,
-  je_id INT REFERENCES journal_entries(id),
-  tag_id INT REFERENCES tags(id)
 );
