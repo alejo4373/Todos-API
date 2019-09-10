ffaa9281f614eb7e0502890e9243bb4cd78c3249
Alejo <alejandrofranco@pursuit.org>
2019-09-10 12:22:07 -0400

    export missing props helper in db index

--- db/index.js
+++ db/index.js
@@ -2,10 +2,12 @@ const Users = require('./users');
 const Todos = require('./todos');
 const Journal = require('./journal');
 const Tags = require('./tags');
+const Helpers = require('./helpers');
 
 module.exports = {
   Users,
   Todos,
   Journal,
-  Tags
+  Tags,
+  Helpers
 }
