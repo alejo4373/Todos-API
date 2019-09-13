66a94962fec509cf60253956c02553670af0f519
Alejo <alejandrofranco@pursuit.org>
2019-09-13 17:32:48 -0400

    enable cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,6 +10,7 @@ var todosRouter = require('./routes/todos');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
